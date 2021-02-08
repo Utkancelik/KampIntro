f3ffb0852f1e74d4301a2cefd6e7d0e00a0e1822
Utkan Çelik <utkancelik@hacettepe.edu.tr>
2021-02-08 20:30:11 +0300

    Created

--- KampIntro/Program.cs
+++ KampIntro/Program.cs
@@ -6,6 +6,43 @@ namespace KampIntro
     {
         static void Main(string[] args)
         {
+            // type safety - tip güvenliği 
+            // Do not repeat yourself - kendini tekrarlama 
+            //değer tutucu, alias
+
+            string kategoriEtiketi = "Kategoriler.";
+            int ogrenciSayisi = 32000;
+            double faizOrani = 1.47;
+            bool sistemeGirisYapmisMi = true;
+            double dolarDun = 7.55;
+            double dolarBugun = 7.55;
+
+
+
+
+
+            if (dolarDun>dolarBugun)
+            {
+                Console.WriteLine("Azalış butonu");
+            }
+            else if (dolarDun<dolarBugun)
+            {
+                Console.WriteLine("Artış Butonu");
+            }
+            else
+            {
+                Console.WriteLine("Değişmedi butonu");
+            }
+
+            if (sistemeGirisYapmisMi == true)
+            {
+                Console.WriteLine("Kullanıcı ayarları butonu");
+
+            }
+            else
+            {
+                Console.WriteLine("Giriş yap butonu");
+            }
             Console.WriteLine("Hello World!");
         }
     }
---
+++ Metotlar/SepetManager.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace Metotlar
+{
+    class SepetManager
+    {
+        //naming convention
+        public void Ekle(Urun urun)
+        {
+            Console.WriteLine(" Tebrikler "+ urun.Adi + " Sepete Eklendi.");
+        }
+
+        public void Ekle2(string urunAdi, string aciklama, double fiyat)
+        {
+            Console.WriteLine(" Tebrikler " + urunAdi + " Sepete Eklendi.");
+        }
+    }
+}
