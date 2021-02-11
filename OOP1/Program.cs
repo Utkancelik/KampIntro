a17a40fb3e38fba442a2d4b5da6e3c63ca26ac93
Utkan Çelik <utkancelik@hacettepe.edu.tr>
2021-02-11 15:33:08 +0300

    20 Ocak Ders Sonu

--- OOP1/Program.cs
+++ OOP1/Program.cs
@@ -6,8 +6,22 @@ namespace OOP1
     {
         static void Main(string[] args)
         {
-            Console.WriteLine("Hello World!");
 
+            Product product1 = new Product();
+            product1.Id = 1;
+            product1.CategoryId = 2;
+            product1.ProductName = "Masa";
+            product1.UnitPrice = 500;
+            product1.UnitsInStock = 3;
+
+
+
+            ProductManager productManager = new ProductManager();
+            productManager.Add(product1);
+            Console.WriteLine(product1.ProductName);
+
+
+            
 
         }
     }
