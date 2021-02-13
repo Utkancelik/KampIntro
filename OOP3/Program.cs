007bdeb717fb16a2e7fa5ccbbf7b112650c6f57d
Utkan Çelik <utkancelik@hacettepe.edu.tr>
2021-02-13 11:32:21 +0300

    23 Ocak Ders Sonu

---
+++ OOP3/Program.cs
@@ -0,0 +1,32 @@
+﻿using System;
+using System.Collections.Generic;
+
+namespace OOP3
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            IKrediManager ihtiyacKrediManager = new IhtiyacKrediManager();
+            IKrediManager tasitKrediManager = new TasitKrediManager();
+            IKrediManager konutKrediManager = new KonutKrediManager();
+
+            ILoggerService fileLoggerService = new FileLoggerService();
+            ILoggerService databaseLoggerService = new DatabaseLoggerService();
+            ILoggerService smsLoggerService = new SmsLoggerService();
+
+            List<ILoggerService> loggers = new List<ILoggerService>
+                {fileLoggerService,databaseLoggerService,smsLoggerService}; 
+            
+
+            BasvuruManager basvuruManager = new BasvuruManager();
+            basvuruManager.BasvuruYap(new EsnafKredisiManager(),loggers);
+
+            List<IKrediManager> krediler = new List<IKrediManager>() {ihtiyacKrediManager,tasitKrediManager,konutKrediManager };
+
+
+            //basvuruManager.KrediOnBilgilendirmesiYap(krediler);
+
+        }
+    }
+}
